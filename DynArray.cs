4db83dcf86f0421d6ba085b990c122976eda183c
Alexander Matkarimov <matkarimovalexander@gmail.com>
2024-04-11 15:49:29 +0300

    Dynamic Array version 1.0

--- DynArray.cs
+++ DynArray.cs
@@ -23,12 +23,6 @@ namespace OOAP_Homework
             count = 0;
             array = new T[START_CAPACITY];
         }
-        // set index метод
-        // find метод
-        // get метод
-        // getbyindex метод
-        // insertAfter
-        // insertBefore
 
         public const int HEAD_NILL = 0;
         public const int HEAD_OK = 1;
@@ -36,9 +30,10 @@ namespace OOAP_Homework
         protected int _headStatus;
         public int GetHeadStatus() => _headStatus;
 
-        // команды
-        // предусловие: массив не пуст; 
-        // постусловие: курсор установлен на первый элемент в массиве
+        // +
+        // Команда
+        // Предусловие: массив не пуст; 
+        // Постусловие: курсор установлен на первый элемент в массиве
         public void Head()
         {
             if (count == 0)
@@ -56,9 +51,10 @@ namespace OOAP_Homework
         protected int _tailStatus;
         public int GetTailStatus() => _tailStatus;
 
-        // команды
-        // предусловие: массив не пуст; 
-        // постусловие: курсор установлен на последний элемент в массиве
+        // +
+        // Команда
+        // Предусловие: массив не пуст; 
+        // Постусловие: курсор установлен на последний элемент в массиве
         public void Tail()
         {
             if (count == 0)
@@ -76,8 +72,10 @@ namespace OOAP_Homework
         protected int _rightStatus;
         public int GetRightStatus() => _rightStatus;
 
-        // предусловие: курсор не указывает на последний элемент 
-        // постусловие: курсор инкрементирован
+        // +
+        // Команда
+        // Предусловие: курсор не указывает на последний элемент 
+        // Постусловие: курсор инкрементирован
         public void Right()
         {
             if (coursor == count -1)
@@ -95,9 +93,10 @@ namespace OOAP_Homework
         protected int _insertBeforeStatus;
         public int GetInsertBeforeStatus() => _insertBeforeStatus;
 
-        // предусловие: список не пуст; 
-        // постусловие: следом за текущим узлом добавлен 
-        // новый узел с заданным значением
+        // +
+        // Команда
+        // предусловие: массив не пуст; 
+        // постусловие: следом за текущим индексом курсора, добавлено новое значение.
         public void InsertBefore(T value)
         {
             if (coursor > count || coursor < 0 || count == capacity)
@@ -129,11 +128,14 @@ namespace OOAP_Homework
         public const int INSERT_AFTER_OK = 1;
         public const int INSERT_AFTER_ERR = 2;
         protected int _insertAfterStatus;
+
         public int GetInsertAfterStatus() => _insertAfterStatus;
 
-        // предусловие: список не пуст; 
+        // +
+        // Команда.
+        // предусловие: массив не пуст; 
         // постусловие: следом за текущим узлом добавлен 
-        // новый узел с заданным значением
+        // новый массив с заданным значением после курсора
         public void InsertAfter(T value)
         {
             if (coursor > count || coursor < 0 || count == capacity)
@@ -166,13 +168,19 @@ namespace OOAP_Homework
         public const int MAKE_ARRAY_ERR = 2;
         protected int _makeArrayStatus;
         public int GetMakeArrayStatus() => _makeArrayStatus;
-        // Сделано
+        // +
         // Команда
         // Пересоздать массив с указанной емкостью
-        // Предусловие
+        // Предусловие емкость должна быть больше емкости по умолчанию
+        // Предусловие емкость должна быть больше количества элементов
         public void MakeArray(int new_capacity)
         {
             if(new_capacity < 0)
+            {
+                _makeArrayStatus = MAKE_ARRAY_ERR;
+                return;
+            }
+            if(count > new_capacity)
             {
                 _makeArrayStatus = MAKE_ARRAY_ERR;
                 return;
@@ -189,20 +197,41 @@ namespace OOAP_Homework
             _makeArrayStatus = MAKE_ARRAY_OK;
         }
 
+        public const int GET_ITEM_BY_INDEX_NILL = 0;
+        public const int GET_ITEM_BY_INDEX_OK = 1;
+        public const int GET_ITEM_BY_INDEX_ERR = 2;
+        protected int _getItemByIndexStatus;
+        public int GetItemByIndexStatus() => _getItemByIndexStatus;
+
+        // +
+        // Запрос
+        // Получить элемент по индексу.
+        // Предусловие: индекс не должен быть ниже нуля
+        // Предусловие: индекс не должен быть больше количества элементов
+        public T GetItemByIndex(int index)
+        {
+            if(index >= capacity || index >= count || index < 0)
+            {
+                _getItemByIndexStatus = GET_ITEM_BY_INDEX_ERR;
+                return default;
+            }
+            _getItemByIndexStatus = GET_ITEM_BY_INDEX_OK;
+            return array[coursor];
+        }
+
         public const int GET_ITEM_NILL = 0;
         public const int GET_ITEM_OK = 1;
         public const int GET_ITEM_ERR = 2;
         protected int _getItemStatus;
         public int GetGetItemStatus() => _getItemStatus;
 
-        // Сделано
+        // +
         // Запрос
-        // Получить элемент по индексу.
-        // Предусловие: индекс не должен быть ниже нуля
-        // Предусловие: индекс не должен быть больше количества элементов
+        // Получить элемент от курсора.
+        // Предусловие: курсор должен находится в границах массива.
         public T GetItem()
         {
-            if(coursor >= capacity || coursor >= count || coursor < 0)
+            if (coursor >= capacity || coursor >= count || coursor < 0)
             {
                 _getItemStatus = GET_ITEM_ERR;
                 return default;
@@ -216,9 +245,10 @@ namespace OOAP_Homework
         public const int APPEND_ERR = 2;
         protected int _getAppendStatus;
         public int GetAppendStatus() => _getAppendStatus;
-        // Сделано
+        // +
         // Команда
-        // Предусловие: список не должен быть переполнен
+        // Добавление элемента в конец массива
+        // Предусловие: массив не должен быть переполнен
         public void Append(T item)
         {
             if (count >= capacity)
@@ -237,6 +267,7 @@ namespace OOAP_Homework
         protected int _extendArrayStatus;
         public int GetExtendArrayStatus() => _extendArrayStatus;
 
+        // +
         // Команда
         // Предусловие: память для нового массива должна быть выделена.
         // Постусловие: массив увеличен
@@ -270,6 +301,7 @@ namespace OOAP_Homework
         protected int _reduceArrayStatus;
         public int GetReduceArrayStatus() => _reduceArrayStatus;
 
+        // +
         // Команда
         // Постусловие: массив уменьшен
         public void ReduceArray()
@@ -302,35 +334,36 @@ namespace OOAP_Homework
         public const int INSERT_ERR = 2;
         protected int _insertStatus;
         public int GetInsertStatus() => _insertStatus;
-        // Команда
-        // Предусловие: индекс находится в границах от 0 до количества элементов.
-        // Предусловие: массив не заполнен полностью.
-        public void Insert(T itm, int index)
-        {
-            if (index > count || index < 0 || count == capacity)
-            {
-                _insertStatus = INSERT_ERR;
-                return;
-            }
-            T[] temp = new T[capacity];
 
-            for (int i = 0; i < index; i++)
-            {
-                temp[i] = array[i];
-            }
+        //// Команда
+        //// Предусловие: индекс находится в границах от 0 до количества элементов.
+        //// Предусловие: массив не заполнен полностью.
+        //public void Insert(T itm, int index)
+        //{
+        //    if (index > count || index < 0 || count == capacity)
+        //    {
+        //        _insertStatus = INSERT_ERR;
+        //        return;
+        //    }
+        //    T[] temp = new T[capacity];
 
-            temp[index] = itm;
+        //    for (int i = 0; i < index; i++)
+        //    {
+        //        temp[i] = array[i];
+        //    }
 
-            for (int i = index + 1; i <= count; i++)
-            {
-                temp[i] = array[i - 1];
-            }
+        //    temp[index] = itm;
 
-            array = temp;
+        //    for (int i = index + 1; i <= count; i++)
+        //    {
+        //        temp[i] = array[i - 1];
+        //    }
 
-            count++;
-            _insertStatus = INSERT_OK;
-        }
+        //    array = temp;
+
+        //    count++;
+        //    _insertStatus = INSERT_OK;
+        //}
 
         public const int REMOVE_NILL = 0;
         public const int REMOVE_OK = 1;
@@ -338,6 +371,7 @@ namespace OOAP_Homework
         protected int _removeStatus;
         public int GetRemoveStatus() => _removeStatus;
 
+        // +
         // Команда
         // Предусловие: индекс находится в границах от 0 до количества элементов.
         // Предусловие: массив не заполнен полностью.
@@ -372,9 +406,9 @@ namespace OOAP_Homework
         protected int _findStatus;
         public int GetFindStatus() => _findStatus;
 
+        // +
         // Команда.
-        // постусловие: курсор установлен на следующий узел 
-        // с искомым значением, если такой узел найден
+        // постусловие: курсор установлен на индекс с найденным значением, если такое найдено.
         public void Find(T value)
         {
             for (int i = coursor; i < count; i++)
@@ -394,7 +428,10 @@ namespace OOAP_Homework
         public const int REPLACE_ERR = 2;
         protected int _replaceStatus;
         public int GetReplaceStatus() => _replaceStatus;
-        // предусловие: список не пуст;
+
+        // +
+        // Команда
+        // предусловие: массив не пуст;
         // предусловие: курсор в границах массива;
         // постусловие: значение текущего индекса заменено на новое
         public void Replace(T value)
@@ -413,12 +450,14 @@ namespace OOAP_Homework
         public const int REMOVE_ALL_OK = 1;
         public const int REMOVE_ALL_ERR = 2;
         protected int _removeAllStatus;
-        public int GetRemoveALLStatus() => _removeAllStatus;
+        public int GetRemoveAllStatus() => _removeAllStatus;
 
+        // +
         // Команда
         // Предусловие: массив не пуст
         // Постусловие: в массиве удалены все узлы с заданным значением
-        public void remove_all(T value)
+        // нет смысла вызывать атомарную функцию Remove постоянно, сложность будет O(N*N)
+        public void RemoveAll(T value)
         {
             var list = new List<T>();
             for (int i = 0; i < array.Length; i++)
@@ -439,7 +478,7 @@ namespace OOAP_Homework
 
 
         // Команда.
-        // постусловие: список очищен от всех элементов
+        // постусловие: массив очищен от всех элементов
         public void Clear()
         {
             count = 0;
